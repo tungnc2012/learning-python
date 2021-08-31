f18de0d5976176b38f00d1778b3ead809411ce49
tungnc2012 <tungnc2012@gmail.com>
2021-08-31 10:13:35 +0000

    if-else simple code

---
+++ if-else-elif.py
@@ -0,0 +1,9 @@
+name = input("Please enter your username ")
+if len(name) <= 5:
+   print("Your name is too short.")
+elif len(name) == 8:
+   print("Your name is 8 characters.")
+elif len(name) >= 8:
+   print("Your name is 8 or more characters.")
+else:
+   print("Your name is short.")
